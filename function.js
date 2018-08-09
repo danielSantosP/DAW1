349a0bd757c3902c713dcac84662d536a1cff391
danielSantosP <39721000+danielSantosP@users.noreply.github.com>
2018-08-09 16:26:49 -0300

    Exercicio6_Lista1
    Resolução do exercicio 6 da lista 1

--- function.js
+++ function.js
@@ -1,17 +1,27 @@
-function createInputAges(){
-	var div = document.getElementById('idAddAges');
-	var input = document.createElement("input");
-	input.type = "text";
-	input.name = "ages[]";
-	div.appendChild(input);
-	div.appendChild(document.createElement("br"));
-};
+function fieldValidation(){
+	var form = document.getElementById("idForm");
+	var name = form.idName;
+	var note1 = form.idNote1;
+	var note2 = form.idNote2;
+	var note3 = form.idNote3;
 
-function createInputNames(){
-	var div = document.getElementById('idAddNames');
-	var input = document.createElement("input");
-	input.type = "text";
-	input.name = "names[]";
-	div.appendChild(input);
-	div.appendChild(document.createElement("br"));
-};
+	if(name.value == ""){
+		alert('Preencha o campo "Nome"');
+
+	}
+
+	if(note1.value == "" ){
+		alert('Preencha o campo "Nota 1"');
+
+	}
+	if(note2.value == ""){
+		alert('Preencha o campo "Nota 2"');
+
+	}
+	if(note3.value == ""){
+		alert('Preencha o campo "Nota 3"');
+
+	}
+
+	return true;
+}
