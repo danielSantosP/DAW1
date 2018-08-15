f9af9af1b40ddf75a5ec147b4b1fef5d7d667d25
danielSantosP <39721000+danielSantosP@users.noreply.github.com>
2018-08-15 18:42:07 -0300

    Exer1Lista2
    Resolução do exercicio 1 da lista 2

--- function.js
+++ function.js
@@ -1,17 +1,47 @@
-function fieldValidation(){
-	var form = document.getElementById("idForm");
-	var name = form.idName;
-	var password = form.idPassword;
+function createInputs(){
+	var div = document.getElementById('createInputs');
+	var label = document.createElement("label");
+	label.innerHTML = "Nome: ";
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "firstName[]";
+	div.appendChild(label);
+	div.appendChild(input);
+	div.appendChild(document.createElement("br"));
 
-	if(name.value == ""){
-		alert('Preencha o campo "Nome"');
+	var label = document.createElement("label");
+	label.innerHTML = "Sobrenome: ";
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "lastName[]";
+	div.appendChild(label);
+	div.appendChild(input);
+	div.appendChild(document.createElement("br"));
 
-	}
+	var label = document.createElement("label");
+	label.innerHTML = "Idade: ";
+	var input = document.createElement("input");
+	input.type = "number";
+	input.name = "age[]";
+	div.appendChild(label);
+	div.appendChild(input);
+	div.appendChild(document.createElement("br"));
 
-	if(password.value == ""){
-		alert('Preencha o campo "Senha"');
+	var label = document.createElement("label");
+	label.innerHTML = "Sexo: ";
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "sex[]";
+	div.appendChild(label);
+	div.appendChild(input);
+	div.appendChild(document.createElement("br"));
 
-	}
-
-	return true;
-}
+	var label = document.createElement("label");
+	label.innerHTML = "Data de nascimento: ";
+	var input = document.createElement("input");
+	input.type = "date";
+	input.name = "date[]";
+	div.appendChild(label);
+	div.appendChild(input);
+	div.appendChild(document.createElement("br"));
+};
--- function.php
+++ function.php
@@ -1,11 +1,27 @@
 <?php
-	if (isset($_POST['submit'])) {
-		$name = $_POST['name'];
-		$mail = $_POST['mail'];
-		$date = $_POST['date'];
-		$optionSelect = $_POST['select'];
 
-		echo "Nome: " . $name . "<br>E-mail: " . $mail . "<br>Data de nascimento: " . $date . "<br>Bandeira do cartão: " . $optionSelect;
+	if (isset($_POST['submit'])) {
+		
+		$person = array(
+			$firstName = $_POST['firstName'],
+			$lastName = $_POST['lastName'],
+			$age = $_POST['age'],
+			$sex = $_POST['sex'],
+			$date = $_POST['date'],
+		);
+
+
+
+		array_multisort($person[4],SORT_ASC, $person[0],SORT_ASC, $person[1],SORT_ASC, $person[2],SORT_ASC, $person[3],SORT_ASC);
+
+		for ($row=0; $row < 5; $row++) { 
+			echo "<ul>";
+			for ($col=0; $col < 5; $col++) { 
+				echo "<li>" . $person[$col][$row] . "</li>";
+			}
+			echo "</ul>";
+		}
+
 	}
-	
- ?>
+
+?>
