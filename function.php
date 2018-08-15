ab32334a801ba02e4556920cbdbb1b62c49f5277
danielSantosP <39721000+danielSantosP@users.noreply.github.com>
2018-08-15 18:46:38 -0300

    Exer3Lista2
    Resolução do exercicio 3 da lista 2

--- function.php
+++ function.php
@@ -1,27 +1,14 @@
 <?php
 
-	if (isset($_POST['submit'])) {
-		
-		$person = array(
-			$firstName = $_POST['firstName'],
-			$lastName = $_POST['lastName'],
-			$age = $_POST['age'],
-			$sex = $_POST['sex'],
-			$date = $_POST['date'],
-		);
+	$archive = fopen('arquivo.txt', 'w+');
+	fwrite($archive, 'Daniel Santos do Patrocinio');
+	fread($archive, filesize('arquivo.txt'));
+	
+
+
+	fclose($archive);
 
 
 
-		array_multisort($person[4],SORT_ASC, $person[0],SORT_ASC, $person[1],SORT_ASC, $person[2],SORT_ASC, $person[3],SORT_ASC);
-
-		for ($row=0; $row < 5; $row++) { 
-			echo "<ul>";
-			for ($col=0; $col < 5; $col++) { 
-				echo "<li>" . $person[$col][$row] . "</li>";
-			}
-			echo "</ul>";
-		}
-
-	}
 
 ?>
